631a5b5dbdda8ee3f67cba14f26fc0dd497fbead
David Dallas <david.dallas@fan-hub.com>
2024-12-01 09:22:43 +0000

    day one added

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using AoC;
-
+﻿using AoC;
 
 var dayOne = new DayOne();
 var dayOneSolution = dayOne.GetTotalDifferenceAndSimilarityScore();
-Console.WriteLine(dayOneSolution);
-
